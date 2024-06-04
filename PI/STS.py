8c4991d22f635c86ac61f3e363e3ee6477e624c2
DiegoFloresRuiz <133287417+DiegoFloresRuiz@users.noreply.github.com>
2024-06-04 13:12:23 -0600

    correccion

--- PI/STS.py
+++ PI/STS.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder='public', template_folder='templates')
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = ""
-app.config['MYSQL_DB'] = "educonnect"
+app.config['MYSQL_DB'] = "STS"
 
 app.secret_key = 'mysecretkey'
 
